075b476da38dc90961ab83b6794ff2b802d7b853
Felix-7 <felix1.kainz@aon.at>
2019-11-14 17:52:37 +0100

    Authenticating the Application
    Well, that was pretty simple and could all be done in the web.php. Nice!
    + Restricted Access to entire platform but the login and the main page
    + Disabled verify-email, registration and manual password reset
    
    What's missing now:
    - GROUPUPLOAD! Super important!
    - Graphics overhaul

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,50 @@ use Illuminate\Http\Request;
 Route::view('/', 'welcome')->name('main');
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::post('/admin/listUpload', 'ListUploadController@store')->name('admin.list.upload');
+//FROM HERE ON, ENTIRE APPLICATION HAS RESTRICTED ACCESS
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::post('/admin/listUpload', 'ListUploadController@store')->name('admin.list.upload');
 
 //MANAGEMENT OF THE APPLICATION
 
-Route::view('/admin/upload', 'admin.settings')->name('admin');
-Route::get('/admin/accounts', 'AccountsController@resetPasswords')->name('admin.accounts');
-Route::get('/admin/{id}/reset', 'AccountsController@resetThisAccount')->name('admin.reset');
+    Route::view('/admin/upload', 'admin.settings')->name('admin');
+    Route::get('/admin/accounts', 'AccountsController@resetPasswords')->name('admin.accounts');
+    Route::get('/admin/{id}/reset', 'AccountsController@resetThisAccount')->name('admin.reset');
 
 //ENTRY ROUTES
 
-Route::resource('disciplines', 'DisciplinesController');
-Route::get('/disciplines/{discipline}/{group}', 'EntryController@startEntries')->name('entry.action');
-Route::get('/disciplines/{discipline}/{group}/{student}/edit', 'EntryController@editEntry')->name('entry.edit');
-Route::match(array('GET', 'POST'),'/disciplines/{discipline}/{group}/{student}/skipFlag={skipFlag}', 'EntryController@nextEntry')->name('entry.next');
-Route::get('/disciplines/{discipline}/{group}/summary', 'EntryController@nextEntry')->name('entry.summary'); //ToDO Route via /summary to finish inputs
-Route::post('/{discipline}/{group}/success', 'ValuesController@store')->name('entry.store');
+    Route::resource('disciplines', 'DisciplinesController');
+    Route::get('/disciplines/{discipline}/{group}', 'EntryController@startEntries')->name('entry.action');
+    Route::get('/disciplines/{discipline}/{group}/{student}/edit', 'EntryController@editEntry')->name('entry.edit');
+    Route::match(array('GET', 'POST'),'/disciplines/{discipline}/{group}/{student}/skipFlag={skipFlag}', 'EntryController@nextEntry')->name('entry.next');
+    Route::get('/disciplines/{discipline}/{group}/summary', 'EntryController@nextEntry')->name('entry.summary'); //ToDO Route via /summary to finish inputs
+    Route::post('/{discipline}/{group}/success', 'ValuesController@store')->name('entry.store');
 
 //STATS ROUTES
 
-Route::get('/stats', 'StatsController@selection')->name('stats.select');
-Route::get('/stats/{mode}', 'StatsController@searchByDiscipline')->name('stats.discipline');
-Route::post('/stats/result', 'StatsController@deliver')->name('stats.deliver');
-Route::post('/stats/detailedSearch', 'StatsController@detailSearch')->name('stats.detail');
-Route::post('/stats/result/final', 'StatsController@deliverDetail')->name('stats.deliverDetail');
-Route::get('/stats/{discipline_id}/{mode}/{gender}/{useAge}/{age}/{class}/{limit}/{upper}/download', 'StatsController@downloadPDF')->name('stats.download');
-Route::get('/stats/latest/download', 'StatsController@downloadLatestPDF')->name('stats.downloadLatest');
+    Route::get('/stats', 'StatsController@selection')->name('stats.select');
+    Route::get('/stats/{mode}', 'StatsController@searchByDiscipline')->name('stats.discipline');
+    Route::post('/stats/result', 'StatsController@deliver')->name('stats.deliver');
+    Route::post('/stats/detailedSearch', 'StatsController@detailSearch')->name('stats.detail');
+    Route::post('/stats/result/final', 'StatsController@deliverDetail')->name('stats.deliverDetail');
+    Route::get('/stats/{discipline_id}/{mode}/{gender}/{useAge}/{age}/{class}/{limit}/{upper}/download', 'StatsController@downloadPDF')->name('stats.download');
+    Route::get('/stats/latest/download', 'StatsController@downloadLatestPDF')->name('stats.downloadLatest');
 
 //AUTH ROUTES
 
+
+    Route::get('/newPassword', 'PersonalPasswordController@newPassword')->name('auth.newPW');
+    Route::post('/passwordSuccess', 'PersonalPasswordController@savePassword')->name('auth.savePW');
+
+
+});
+
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false,
+]);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -52,7 +67,5 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 Route::get('/logout', 'Auth\LoginController@logout');
-Route::get('/newPassword', 'PersonalPasswordController@newPassword')->name('auth.newPW');
-Route::post('/passwordSuccess', 'PersonalPasswordController@savePassword')->name('auth.savePW');
 
-Auth::routes();
+
